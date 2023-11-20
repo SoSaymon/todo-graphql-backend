9f06927cc9da2f4a86a5f04ad840dbfe0a47c764
SoSaymon <szymon.chirowski@protonmail.com>
2023-11-20 18:43:42 +0100

    fix(app): fix no CORS header

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from graphene import Schema
+from starlette.middleware.cors import CORSMiddleware
 from starlette_graphene3 import GraphQLApp, make_playground_handler
 
 from app.gql.mutations import Mutation
@@ -7,6 +8,14 @@ from app.gql.queries import Query
 from app.utils.database import create_database
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 schema = Schema(query=Query, mutation=Mutation)
 
